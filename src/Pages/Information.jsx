ec6c43afe8a730f2c72cdd042f0fd7268fb7a59c
erickcampaz <erickcampaz@MacBook-Pro-de-erickcampaz.local>
2024-01-03 19:52:37 -0600

    Agregando estilo de pay y eliminar eliminar localSotage

--- src/Pages/Information.jsx
+++ src/Pages/Information.jsx
@@ -8,10 +8,10 @@ function Information() {
 
   const Onclickpage =()=>{
 
-    // eliminar todo el localStorage
+
     localStorage.clear();
    
-    alert("Gracias Por tu Compra, Thanks for your purchase")
+    window.alert("Gracias Por tu Compra, Thanks for your purchase")
     window.location= '/'
    
    }
@@ -23,59 +23,33 @@ function Information() {
 
     <div className='Container-info'>
       <p className='P-informaPa'>Information of Pay</p>
-      <div className='informortion-Container2'>
-       
-    
-  <ul className='ul-infor'>
-    <li>
-      <label for="name">Nombre / Name:</label>
-      <input type="text" class="form-control" id="name" name="user_name" placeholder='Jaime'/>
-    </li>
-    <li>
-      <label for="name">Apellido / lest Name:</label>
-      <input type="text"class="form-control" id="name" name="user_name" placeholder='Musk'/>
-    </li>
-    <li>
-      <label for="mail">Pais / Country:</label>
-      <input type="email" class="form-control1" id="mail" name="user_mail" placeholder='colombia' />
-    </li>
-    <li>
-      <label for="state">Departamento / State</label>
-      <input type="email"class="form-control" id="mail" name="user_mail" placeholder='TX, valle'/>
-    </li>
-    <li>
-      <label for="addres">Direccion / Addres</label>
-      <input type="email"class="form-control" id="mail" name="user_mail" placeholder='8822, Monar glex 77855' />
-    </li>
+       <form class="form" >
 
-    <li>
-    <li>
+   <label for="name">Nombre / Name:</label>
+  <input type="text"  placeholder="Jhon"/>
+  <label for="name">Apellido / lest Name:</label>
+  <input type="text" placeholder='Brox' /> 
+  <label for="name">Pais / Coutry:</label>
+  <input type="text" placeholder='Colombia' />
+  <label for="name">Departamento / State:</label>
+  <input type="email" placeholder="Valle" />
+  <label for="name">Correo Electr / Email:</label>
   
-      <label for="cardNumber">Numero Tarjeta / Card Number</label>
-       <input type="number" class="form-control2" id="cardNumber" placeholder='855-8855-4555'/>
-      
-    </li>
-    <li>
-  
-  <label for="cardNumber">Cvv</label>
-   <input  type="number" class="form-control3" id="cardNumber" placeholder='585'/>
-  
-</li>
-      <label for="Mesagge">Correo electrónico / Email:</label>
-      <input type="email" class="form-control" id="mail" name="user_mail"placeholder='store@gamil.com' />
-    </li>
-    <li>
-      <label for="msg">Mensaje / Message:</label>
-      <textarea id="msg" className='msg' name="user_message" placeholder='Me house de color greed or mi casa es de color verde'></textarea>
-    </li>
-    
-    
-  </ul>
+ <input type="email" placeholder="store@gamil.com" />
 
+ <label className='Card' for="name"><h1 className='h2-lables'>Tarjeta / Card:</h1>
 
-      </div>
+ <input className='CardNum' type="Number"  placeholder="888-5689-5587" />
+ 
+ <input className='CardCvv' type="Number" placeholder="Cvv" />
+</label>
+<label for="name">Dirrecion / Anddres:</label>
+ <input type="text" placeholder='8822, Monar glex 77855' />
+ <label for="name">Mensaje / Msg:</label>
+ <input type="msg" placeholder='Hello, Nedd...' />
+</form>
      
-        <button className='btn-pay' onClick={Onclickpage}>Pay</button>
+        <button className='bton-form-button' onClick={Onclickpage}>Pay</button>
     </div>
     <br/>
     <Footer/>   
